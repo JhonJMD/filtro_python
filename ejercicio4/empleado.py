75f2283b036f1921ac0afad1c576e2fa80a899cb
Jhon Jario <152086681+JhonJMD@users.noreply.github.com>
2024-03-06 13:54:22 -0500

    final filtro

--- ejercicio4/empleado.py
+++ ejercicio4/empleado.py
@@ -60,28 +60,38 @@ def addEmpleado(empleados : dict):
                     break
         empleado['nombre'] = input('Nombre del Empleado: ')
         empleado['cargo'] = input('Ingrese el cargo del empleado (Almacenista, Jefe IT, Administrador, Mensajero, Gerente)')
-        empleado['salario'] = validacionFloat(message='Ingrese el salario del Empleado: ')
         sc.cleanScreen()
         empleados.update({id : empleado}) #Actualiza el empleados con el contenido dado
         file.updateFile('empleados.json', empleados)
         isaddempleado = otherEmpleado(isaddempleado, empleados)
 
 def calcularVal(empleados : dict, nomina : dict):
+    idP = 0
     isaddempleado = True
     while isaddempleado:
         pagado = {
+            'idP' : 0,
             'diasTrabajados' : 0.0,
             'horasExtras' : 0.0,
             'valorDia' : 0.0,
             'descuentoCafeteria' : 0.0,
             'cuotaPrestamo' : 0.0
         }
-        
-        pagado['nombre'] = input('Nombre del Empleado: ')
-        pagado['cargo'] = input('Ingrese el cargo del pagado (Almacenista, Jefe IT, Administrador, Mensajero, Gerente)')
-        pagado['salario'] = validacionFloat(message='Ingrese el salario del Empleado: ')
-        sc.cleanScreen()
-        nomina.update({id : pagado}) #Actualiza el nomina con el contenido dado
-        file.updateFile('nomina.json', nomina)
-        isaddempleado = otherEmpleado(isaddempleado, nomina)
-        
+        if len(empleados) == 0:
+            print('No hay ningun empleado registrado')
+            sc.pauseScreen()
+        else:
+            idP+=1
+            id = validacionInt(message='Ingrese el id del empleado a calcular: ')
+            for key in empleados.keys():
+                if key == str(id):
+                    pagado['diasTrabajados'] = validacionFloat(message='Ingrese los dias trabajados: ')
+                    pagado['horasExtras'] = (validacionFloat(message='Ingrese el valor del dia: ')*5500)
+                    pagado['descunetoCafeteria'] = validacionFloat(message='Ingrese el valor de descuento por cafeteria: ')
+                    pagado['cuotaPrestamo'] = validacionFloat(message='Ingrese la cuota de descuento: ')
+                    sc.cleanScreen()
+                    nomina.update({idP : pagado}) #Actualiza el nomina con el contenido dado
+                    file.updateFile('nomina.json', nomina)
+                    isaddempleado = otherEmpleado(isaddempleado, nomina)
+                    
+            
